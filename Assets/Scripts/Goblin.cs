6af2eef5d4b14ec889b492e4e06d3bc02aee24aa
Fabrizio2102 <f.battistella@correo.unimet.edu.ve>
2024-03-20 16:43:32 -0400

    Feat: goblin version 1
    Nuevo boss, version 1

--- Assets/Scripts/Goblin.cs
+++ Assets/Scripts/Goblin.cs
@@ -11,12 +11,27 @@ public class Goblin : MonoBehaviour
 
     [Header("Health")]
     [SerializeField] public float health;
-    
+
+    [Header("Damage")]
+    [SerializeField] public Transform attackControl1;
+    [SerializeField] Vector2 attackArea1;
+    [SerializeField] public float damage1;
+    [SerializeField] public Transform attackControl2;
+    [SerializeField] public float radioAttack2;
+    [SerializeField] public float damage2;
+
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float distance = Vector2.Distance(transform.position, PlayerController.Instance.transform.position);
+        anim.SetFloat("Distance", distance);
     }
 
     public void takeDamage(float damage)
@@ -42,9 +57,35 @@ public class Goblin : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Attack1()
     {
-        
+        Collider2D[] objectsToHit = Physics2D.OverlapBoxAll(attackControl1.position, attackArea1, 0);
+
+        foreach (Collider2D colision in objectsToHit)
+        {
+            if(colision.CompareTag("Player"))
+            {
+                PlayerController.Instance.TakeDamage(damage1);
+            }
+        }
+    }
+
+    public void Attack2()
+    {
+        Collider2D[] objetos = Physics2D.OverlapCircleAll(attackControl2.position, radioAttack2);
+        foreach(Collider2D colision in objetos)
+        {
+            if(colision.CompareTag("Player"))
+            {
+                PlayerController.Instance.TakeDamage(damage2);
+            }
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackControl1.position, attackArea1);
+        Gizmos.DrawWireSphere(attackControl2.position, radioAttack2);
     }
 }
